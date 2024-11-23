e423057bd51f326ed176e81acf26b42cc8c3419a
swedhearth <hubertswed@gmail.com>
2024-11-23 16:10:04 +0000

    MobileOptimisation_08
    Improved visuals and animation in form view

--- assets/src/native/ui.js
+++ assets/src/native/ui.js
@@ -1,7 +1,7 @@
-/* 'frequent_0.031_GitHub' */
+/* 'frequent_0.032_GitHub' */
 
 function Interface(thisApp){
-    let tempVer = "frequent_0.031_GitHub";
+    let tempVer = "frequent_0.032_GitHub";
     "use strict";
     if(developerMode) console.log("initiate Interface");
     
