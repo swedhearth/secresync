c151ff426934f3bedaaa27bb9733886174b3d601
swedhearth <hubertswed@gmail.com>
2024-11-30 16:55:44 +0000

    MobileOptimisation_31

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 const appCaches = [
     {
-        name: 'frequent_0.060_GitHub',
+        name: 'frequent_0.061_GitHub',
         urls: [
             "./",
             "./secresync.webmanifest",
